cc7f7b51caa828ad05c83f1f4dd337a62a064610
Trey Carey <treyacarey@icloud.com>
2018-09-13 13:13:57 -0500

    Add Title and Logo
    Added and positioned Title and Logo to the main view, line 35 of loginScreen is a little hacky, may need to update later.

--- src/loginScreen.java
+++ src/loginScreen.java
@@ -1,8 +1,13 @@
 //Made by Trey Carey | 9.11.18
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 public class loginScreen extends Application{
@@ -11,9 +16,28 @@ public class loginScreen extends Application{
 	public void start(Stage primaryStage) throws Exception {
 		
 		BorderPane borderPane = new BorderPane();
+		borderPane.setId("background");
 		
 		loginScreen = new Scene(borderPane, 800, 600);
+		loginScreen.getStylesheets().add("/files/styles.css");
 		
+		
+		//TITLE\\
+		Label title = new Label(applicationSettings.getApplicationName());
+		title.setId("titles");
+		BorderPane.setAlignment(title, Pos.CENTER);
+		borderPane.setTop(title);
+		
+		//POK LOGO\\
+		ImageView pokLogo = new ImageView("/images/POK LOGO.png");
+		HBox bottomHBox = new HBox();
+		//Code below is a little hacky but it gets the job done.
+		bottomHBox.setPadding(new Insets(0, 0, 22, loginScreen.getWidth() / 2 - pokLogo.boundsInParentProperty().get().getWidth() / 2));
+		bottomHBox.getChildren().addAll(pokLogo);
+		BorderPane.setAlignment(bottomHBox, Pos.CENTER);
+		borderPane.setBottom(bottomHBox);
+		
+		//STAGE STUFF\\
 		primaryStage.setScene(loginScreen);
 		primaryStage.setResizable(false);
 		primaryStage.setTitle(applicationSettings.getApplicationName());
