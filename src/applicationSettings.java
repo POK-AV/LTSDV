b855459284fb378e2984e390d1a40de844e435c8
Trey Carey <treyacarey@icloud.com>
2018-09-16 12:39:16 -0500

    Moved SD Generation, Added Ability to update SD Text
    Moved SD Generation to applicationSettings

--- src/applicationSettings.java
+++ src/applicationSettings.java
@@ -5,6 +5,14 @@ import java.io.InputStream;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Screen;
+import javafx.stage.Stage;
+
 public class applicationSettings {
 	
 	static String name = "Lower Thirds SDV";
@@ -42,4 +50,55 @@ public class applicationSettings {
 		return 4.0;
 	}
 	
+	static Label sdText = null;
+	
+public static Stage startSD(Stage primaryStage) {
+		
+		System.out.println(Screen.getScreens());
+		
+		Stage sdStage = new Stage();
+		sdStage.setFullScreen(true);
+		
+		BorderPane borderPane = new BorderPane();
+		borderPane.setId("SDbackground");
+		
+		borderPane.getStylesheets().add("/files/styles.css");
+		
+		Scene mainDashboard = new Scene(borderPane, 800, 600);
+		
+		sdText = new Label("This is a Stage Display test, I have to ensure that this text is long to make sure that it is correctly formatting on the screen and not clipping whatsoever.");
+		BorderPane.setAlignment(sdText, Pos.CENTER);
+		sdText.setPadding(new Insets(50));
+		sdText.setId("sdText");
+		sdText.setWrapText(true);
+		borderPane.setBottom(sdText);
+		
+		
+		sdStage.setTitle(applicationSettings.getApplicationName() + " - VIEWER");
+		sdStage.setResizable(false);
+		sdStage.setScene(mainDashboard);
+		sdStage.show();
+		
+		/*
+		 * Below is an attempt to fix a bug in which the mainDisplay continues to
+		 * fullscreen itself even after being implicitly told not too, who even
+		 * knows why computers do what they do anymore. Maybe i'm just missing something
+		 * obvious. Anyways this seems to work for now, i'll have to do some more
+		 * studying later and see if there's something i'm missing that would cause this.
+		 */
+		
+		//TODO: Refactor this, make it nice and non-hacky
+		primaryStage.setFullScreen(false);
+		primaryStage.setMaxHeight(600);
+		primaryStage.setMaxWidth(800);
+		primaryStage.setFullScreen(false);
+		
+		
+		return sdStage;
+	}
+
+	public static void updateSDText(String newText) {
+		sdText.setText(newText);
+	}
+
 }
--- src/mainDashboard.java
+++ src/mainDashboard.java
@@ -20,7 +20,7 @@ public abstract class mainDashboard extends Application{
 		
 		Stage primaryStage = new Stage();
 		
-		Stage stageDisplay = startSD(primaryStage);
+		Stage stageDisplay = applicationSettings.startSD(primaryStage);
 		
 		BorderPane borderPane = new BorderPane();
 		borderPane.setId("background");
@@ -68,54 +68,11 @@ public abstract class mainDashboard extends Application{
 		primaryStage.setFullScreen(false); //Has to be here to fix both Stages going FullScreen
 		primaryStage.show();
 		
+		//applicationSettings.updateSDText("This is a text change test.");
+		
 		primaryStage.setOnCloseRequest(event -> {
 		    stageDisplay.close();
 		});
-	}
-	
-	public static Stage startSD(Stage primaryStage) {
-		
-		System.out.println(Screen.getScreens());
-		
-		Stage sdStage = new Stage();
-		sdStage.setFullScreen(true);
-		
-		BorderPane borderPane = new BorderPane();
-		borderPane.setId("SDbackground");
-		
-		borderPane.getStylesheets().add("/files/styles.css");
-		
-		Scene mainDashboard = new Scene(borderPane, 800, 600);
-		
-		Label sdText = new Label("This is a Stage Display test, I have to ensure that this text is long to make sure that it is correctly formatting on the screen and not clipping whatsoever.");
-		BorderPane.setAlignment(sdText, Pos.CENTER);
-		sdText.setPadding(new Insets(50));
-		sdText.setId("sdText");
-		sdText.setWrapText(true);
-		borderPane.setBottom(sdText);
-		
-		
-		sdStage.setTitle(applicationSettings.getApplicationName() + " - VIEWER");
-		sdStage.setResizable(false);
-		sdStage.setScene(mainDashboard);
-		sdStage.show();
-		
-		/*
-		 * Below is an attempt to fix a bug in which the mainDisplay continues to
-		 * fullscreen itself even after being implicitly told not too, who even
-		 * knows why computers do what they do anymore. Maybe i'm just missing something
-		 * obvious. Anyways this seems to work for now, i'll have to do some more
-		 * studying later and see if there's something i'm missing that would cause this.
-		 */
-		
-		//TODO: Refactor this, make it nice and non-hacky
-		primaryStage.setFullScreen(false);
-		primaryStage.setMaxHeight(600);
-		primaryStage.setMaxWidth(800);
-		primaryStage.setFullScreen(false);
-		
-		
-		return sdStage;
 	}
 
 }
